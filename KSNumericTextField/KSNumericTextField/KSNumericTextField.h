d684cbf0fca80ecf54265dce5984a5eff4e07a50
Lee Kah Seng <yeos_lee@yahoo.com.sg>
2018-03-17 21:45:52 +0800

    Added project

---
+++ KSNumericTextField/KSNumericTextField/KSNumericTextField.h
@@ -0,0 +1,19 @@
+//
+//  KSNumericTextField.h
+//  KSNumericTextField
+//
+//  Created by Lee Kah Seng on 17/03/2018.
+//  Copyright © 2018 Lee Kah Seng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSNumericTextField.
+FOUNDATION_EXPORT double KSNumericTextFieldVersionNumber;
+
+//! Project version string for KSNumericTextField.
+FOUNDATION_EXPORT const unsigned char KSNumericTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSNumericTextField/PublicHeader.h>
+
+
---
+++ KSNumericTextFieldExample/KSNumericTextFieldExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KSNumericTextFieldExample
+//
+//  Created by Lee Kah Seng on 17/03/2018.
+//  Copyright © 2018 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
